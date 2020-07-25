37907820dd924326f9fec107c745953b29b14bb9
dhens <dawsonhensel@gmail.com>
2020-07-25 15:31:13 -0700

    upgraded index to include function. as this is a big innovation, we are moving our version to 1.1.0

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello, world!');
+// Declare the message we want to broadcast
+let message = 'Hello, world!';
+
+// Log any message we provide to the function
+const sayHello = message => {
+    console.log(message);
+}
+
+// Execute our sayHello function to log our message
+sayHello(message);
